cf02983c53df169fbdbaaa11ac7f65964ffecd9c
sarahsauzullo <sarahsauzullo@gmail.com>
2025-05-23 15:43:02 +0200

    modifica controller, utente, creazione interface

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,5 +1,28 @@
 package controller;
 
+import model.Bacheca;
+import java.util.*;
+import model.*;
+
+
 public class Controller {
+    private ArrayList<Bacheca> bachecaList;
+    public Controller() {this.bachecaList= new ArrayList<Bacheca>();}
+
+    public void addBacheca(Bacheca b){
+        this.bachecaList.add(b);}
+
+    public ArrayList<Bacheca> getBacheca(int i) {
+        return this.bachecaList.get(i);
+    }
+    public ArrayList<Bacheca> removeBacheca(int i) {
+        return this.bachecaList.remove(i);
+    }
+    public ArrayList<Bacheca> getFirstBacheca() {
+        return bachecaList.size();
+    }
+    public ArrayList<Bacheca> getBachece(String type) {
+
+    }
 
 }
--- src/main/java/model/Utente.java
+++ src/main/java/model/Utente.java
@@ -54,12 +54,6 @@ public class Utente {
 
     }
 
-
-
-
-
-
-
     public String getPassword() {
         return password;
     }
