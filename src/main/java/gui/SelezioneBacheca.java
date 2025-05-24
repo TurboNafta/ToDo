c40b94bce8622d54ae502690a61dbc89361c8de8
sarahsauzullo <sarahsauzullo@gmail.com>
2025-05-24 14:38:44 +0200

    modifica gui

--- src/main/java/gui/SelezioneBacheca.java
+++ src/main/java/gui/SelezioneBacheca.java
@@ -15,14 +15,13 @@ public class SelezioneBacheca {
     private JButton buttonCerca;
     private JTable table1;
     private JButton buttonAdd;
-    private DefaultTableModel tableModel;
 
     public static JFrame frameBacheca, frameChiamante;
     private Controller controller;
 
     // NON CI SERVE IL MAIN IN QUANTO NON È LA PRIMA PAGINA AD ESSERE MOSTRATA
 
-    public SelezioneBacheca(Controller controller, JFrame callFrame){
+    public SelezioneBacheca(Controller controller, JFrame callFrame) {
         this.controller = controller;
 
         frameChiamante = callFrame;
@@ -32,23 +31,12 @@ public class SelezioneBacheca {
         this.comboBacheca.addItem("Tempo Libero");
         this.comboBacheca.addItem("Università");
 
+
         frameBacheca = new JFrame("Bacheca");
         frameBacheca.setContentPane(cercaBachecaPanel);
         frameBacheca.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frameBacheca.pack();
         frameBacheca.setVisible(true);
-
-        // Combo con i tipi di bacheca
-        comboBacheca.addItem("Università");
-        comboBacheca.addItem("Lavoro");
-        comboBacheca.addItem("Tempo Libero");
-
-        // Modello tabella
-        tableModel = new DefaultTableModel(new Object[]{"Titolo", "Descrizione", "Data Scadenza"}, 0);
-        table1.setModel(tableModel);
-
-
-
         buttonAdd.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
